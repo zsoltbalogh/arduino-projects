75d37c91a00dac985cc5de0c2d6804a48e3dc907
Zsolt Balogh <bzz@bzz.hu>
2022-02-11 09:27:19 +0100

    Integrating servo for the gate, speaker; automatic light change detection to work in various environments

--- railway_crossing/railway_crossing.ino
+++ railway_crossing/railway_crossing.ino
@@ -1,16 +1,24 @@
+#include <Servo.h>
+
 const int SENSOR_1_PIN = A0;
 const int SENSOR_2_PIN = A1;
+const int SERVO_PIN = A2;
 
 const int LED_WHITE_PIN = 3;
 const int LED_RED_LEFT_PIN = 4;
 const int LED_RED_RIGHT_PIN = 5;
+const int SPEAKER_PIN = 8;
 
 const int BLINK_DELAY = 400;
 
-const int LIGHT_THRESHOLD = 50;
+const float LIGHT_CHANGE_THRESHOLD = 0.2;
+
+Servo gateServo;
 
 int sensor1Value = 0;
 int sensor2Value = 0;
+int prevSensor1Value = 0;
+int prevSensor2Value = 0;
 int vonat = 0;
 
 void setup() {
@@ -18,26 +26,48 @@ void setup() {
   pinMode(LED_WHITE_PIN, OUTPUT);
   pinMode(LED_RED_LEFT_PIN, OUTPUT);
   pinMode(LED_RED_RIGHT_PIN, OUTPUT);
+  pinMode(SPEAKER_PIN, OUTPUT);
+
+  gateServo.attach(SERVO_PIN);
+
+  prevSensor1Value = analogRead(SENSOR_1_PIN);
+  prevSensor2Value = analogRead(SENSOR_2_PIN);
+}
+
+float percentage(int previous, int actual) {
+  return 1.0*(actual-previous)/previous;
 }
 
 void loop() {
   sensor1Value = analogRead(SENSOR_1_PIN);
   sensor2Value = analogRead(SENSOR_2_PIN);
 
-  Serial.print("sensor 1: ");
-  Serial.println(sensor1Value);
-  Serial.print("sensor 2: ");
-  Serial.println(sensor2Value);
+  float change1 = percentage(prevSensor1Value, sensor1Value);
+  float change2 = percentage(prevSensor2Value, sensor2Value);
+  prevSensor1Value = sensor1Value;
+  prevSensor2Value = sensor2Value;
 
-  
-  if (sensor1Value < LIGHT_THRESHOLD) {  
+  Serial.print("sensor 1: ");
+  Serial.print(sensor1Value);
+  Serial.print("(");
+  Serial.print(change1*100);
+  Serial.print("%)");
+  Serial.print(" sensor 2: ");
+  Serial.print(sensor2Value);
+  Serial.print("(");
+  Serial.print(change1*100);
+  Serial.println("%)");
+
+  if (change1 < -1*LIGHT_CHANGE_THRESHOLD) {
     vonat = 1;
+    gateServo.write(0);
   }
-  if (sensor2Value < LIGHT_THRESHOLD && vonat == 1) {
+  if (change2 < -1*LIGHT_CHANGE_THRESHOLD && vonat == 1) {
      vonat = 2;
   }
-  if (sensor2Value > LIGHT_THRESHOLD && vonat == 2) {
+  if (change2 > LIGHT_CHANGE_THRESHOLD && vonat == 2) {
      vonat = 0;
+     gateServo.write(90);
   }
 
   if (vonat == LOW) {
@@ -53,9 +83,11 @@ void loop() {
     digitalWrite(LED_WHITE_PIN, LOW);
     digitalWrite(LED_RED_LEFT_PIN, LOW);
     digitalWrite(LED_RED_RIGHT_PIN, HIGH);
+    tone(SPEAKER_PIN, 262);
     delay(BLINK_DELAY);
     digitalWrite(LED_RED_LEFT_PIN, HIGH);
     digitalWrite(LED_RED_RIGHT_PIN, LOW);
+    noTone(SPEAKER_PIN);
     delay(BLINK_DELAY);
   }
 }
